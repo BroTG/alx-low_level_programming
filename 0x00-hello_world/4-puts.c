02e10a141ec22ecc589f91017962dee599ab69e7
Eze ThankGod <thankgodeze6@gmail.com>
2023-12-30 16:29:52 -0800

    putchar

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *
+ *printing with putchar
+ */
+int main(void)
+{
+	char prog="Programming is like building a multilingual puzzle";
+putchar(prog);
+putchar('\n');
+return(0);
+}
