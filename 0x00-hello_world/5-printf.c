eea64b3e199ec32b89a8c989262338787e64a17a
Eze ThankGod <thankgodeze6@gmail.com>
2024-01-02 01:12:34 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*entrance point main
+*
+*using printf
+*always return 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+
+
+}
