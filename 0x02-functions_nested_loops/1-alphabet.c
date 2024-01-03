77ea57ff7e2ba9a72ad5e1db0b48f513157d685b
Eze ThankGod <thankgodeze6@gmail.com>
2024-01-03 14:56:26 -0800

    lowercase alph

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - Entrance point main
+*
+*Explanation: function to display lowercase alphabets
+*parameter: function print_alphabet and putchar
+*
+*Return: always return 0
+*/
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,12 @@
-void _putchar ()
+void print_alphabet(void)
+{
+	char alph;
+
+	for ( alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+
+	}
+	putchar('\n');
+
+}
