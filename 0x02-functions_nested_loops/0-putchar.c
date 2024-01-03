5a73f2626138d72942eb7481b35d21515a65a966
Eze ThankGod <thankgodeze6@gmail.com>
2024-01-03 14:08:08 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - Entrance point main
+*
+*Explanation: to print _putchar
+*parameter: using putchar
+*
+*Return: Always return 0
+*/
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar(92);
+	putchar(110);
+
+	return (0);
+
+}
