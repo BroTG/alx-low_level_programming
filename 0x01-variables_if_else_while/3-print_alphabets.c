87cc6e46de6ccec2adeb433a7cf69c546cdff77d
Eze ThankGod <thankgodeze6@gmail.com>
2024-01-07 13:55:19 -0800

    display lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*main: - Entrance point main
+*
+*Explanation: to display lowercase and uppercase letters
+*parameter: for and putchar
+*
+*Return: always return 0
+*/
+int main(void)
+{
+	char lower, upper;
+
+	for (lower = 'a'; lower <= 'z'; ++lower)
+	{
+		putchar(lower);
+
+	}
+	for (upper = 'A'; upper <= 'Z'; ++upper)
+	{
+		putchar(upper);
+
+	}
+	putchar('\n');
+	return (0);
+
+}
