32461b624c0ad1a75ccb6825c9ca6be0476f08a7
Eze ThankGod <thankgodeze6@gmail.com>
2024-01-07 13:38:05 -0800

    lowercase alph

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entrance point
+*
+Explanation: to display lowercase alphabets with putchar
+*parameter: putchar
+*
+*Return: Always return 0
+*/
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; ++alph)
+	{
+		putchar(alph);
+
+	}
+	putchar('\n');
+
+	return (0);
+
+}
