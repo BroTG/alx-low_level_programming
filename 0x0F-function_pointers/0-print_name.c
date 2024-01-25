f3b4cbd361bbc06cca04a6e4b7102a4d9cf012ee
Eze ThankGod <thankgodeze6@gmail.com>
2024-01-24 20:35:43 -0800

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+*print_name - prints a name
+*@name: name of a person
+*
+*Return: Nothing.
+*/
+void print_name(char *name, void (*f)(char *));
+{
+	f(name);
+}
+/**
+*f - displays the name
+*@name: name of the person
+*
+*Return: Nothing
+*/
+void f_function(char *str)
+{
+	printf("Name: %s\n", str);
+}
+
+/**
+*main - Entrance point main
+*
+*Explanation: to print a name
+*parameter: function pointer
+*
+*Return: always return 0
+*/
+int main(void)
+{
+char aName[];
+
+aName[] = "Tycoon";
+print_name(aName, f_function);
+
+return (0);
+//reference: ChatGPT
+}
+
