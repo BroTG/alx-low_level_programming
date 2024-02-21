789ac4faba3fa91d9aff90b9ec4c307b2b3d2022
Eze ThankGod <thankgodeze6@gmail.com>
2024-02-21 01:29:34 -0800

    10x lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-#include "1-alphabet.c"
 
 /**
 *print_alphabet_x10 - prints lowercase alphabets 10 times
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,16 @@
 #define LOWCASE
 #include <stdio.h>
 
-void print_alphabet(void);
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <='z'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+
+}
 
 #endif
